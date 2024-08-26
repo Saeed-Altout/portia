1388bcffed2b2c7c924671423f75c0ee667a5274
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-26 17:07:13 +0300

    1- add button, sheet and navigation-menu components from shadcn/ui. 2- create navbar

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,7 +1,14 @@
+import { Navbar } from "./_layouts/navbar";
+
 export default function SiteLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
