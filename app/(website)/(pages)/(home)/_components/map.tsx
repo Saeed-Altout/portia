ff2f57078d4e4572425a6b21500bab6ab1f8b005
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-27 17:02:27 +0300

    update style map section.

--- app/(website)/(pages)/(home)/_components/map.tsx
+++ app/(website)/(pages)/(home)/_components/map.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 
-import { mapLg } from "@/app/(website)/assets/images";
-
 export const Map = () => {
   return (
-    <section className="container-section pt-10 md:h-[600px]">
-      <Image src={mapLg} alt="Map" className="w-full h-full object-contain" />
+    <section className="max-container">
+      <div className="relative h-[250px] md:h-[450px] lg:h-[800px]">
+        <Image
+          src="/images/map.png"
+          alt="Map"
+          className="w-full h-full object-contain"
+          fill
+        />
+      </div>
     </section>
   );
 };
