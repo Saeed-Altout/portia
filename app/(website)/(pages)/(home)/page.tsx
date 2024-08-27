f52acb0959d31e4e71dee149756fd2f317b83548
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-27 21:51:54 +0300

    add separator, accordion and carousel components from shadcn/ui.

--- app/(website)/(pages)/(home)/page.tsx
+++ app/(website)/(pages)/(home)/page.tsx
@@ -15,3 +15,13 @@ export default function HomePage() {
     </main>
   );
 }
+
+// TODO: Update style hading section in all screens.
+// TODO: Create heading and card component.
+// TODO: Add interface to all cards features, plans and statistic.
+// TODO: Create index.ts in components file and export all sections from on file.
+// TODO: Add pricing text and Most popular arrow in plans section.
+// TODO: Edit style card of plans section.
+// TODO: Import fonts as files to fix changes size.
+// TODO: Add line when above each card in statistics section in mobile screen.
+// TODO: Add line effect under Mobile Proxies in hero section.
