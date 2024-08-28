18e7956c9c621647677d62fa55569f73f5b32f12
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-28 13:16:36 +0300

    update style statistic card

--- app/(website)/(pages)/(home)/_components/statistic.tsx
+++ app/(website)/(pages)/(home)/_components/statistic.tsx
@@ -12,16 +12,16 @@ import { statisticData } from "@/app/(website)/constants";
 export const Statistic = () => {
   return (
     <section className="max-container section">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
         {/* TODO: Update data from API */}
         {statisticData.map(({ value, name, description }, index) => (
           <Card key={index} className="shadow-none border-none">
-            <CardHeader>
-              <CardTitle className="text-primary font-bold lg:text-[60px]">
+            <CardHeader className="px-0">
+              <CardTitle className="text-primary font-bold lg:text-5xl">
                 {value}
               </CardTitle>
             </CardHeader>
-            <CardContent className="space-y-1">
+            <CardContent className="space-y-1 px-0">
               <h3 className="font-medium text-black-primary text-[18px]">
                 {name}
               </h3>
