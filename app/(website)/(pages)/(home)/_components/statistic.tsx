a9371b347b7101bafacba3b37fa0a620133fdc03
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-28 20:29:54 +0300

    complete statistic section

--- app/(website)/(pages)/(home)/_components/statistic.tsx
+++ app/(website)/(pages)/(home)/_components/statistic.tsx
@@ -1,31 +1,21 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { statisticData } from "@/app/(website)/constants";
 
 export const Statistic = () => {
   return (
     <section className="max-container section">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* TODO: Update data from API */}
         {statisticData.map(({ value, name, description }, index) => (
           <Card key={index} className="shadow-none border-none">
             <CardHeader className="px-0">
-              <CardTitle className="text-primary font-bold lg:text-5xl">
+              <CardTitle className="text-primary font-semibold text-5xl lg:text-6xl">
                 {value}
               </CardTitle>
             </CardHeader>
             <CardContent className="space-y-1 px-0">
-              <h3 className="font-medium text-black-primary text-[18px]">
-                {name}
-              </h3>
-              <p className="text-gray-primary text-base">{description}</p>
+              <h3 className="font-medium text-lg">{name}</h3>
+              <p>{description}</p>
             </CardContent>
           </Card>
         ))}
