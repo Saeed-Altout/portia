96d0007117297cf10a29ec3e2c2df1f110c98f6c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-31 16:24:16 +0300

    1- update import from long to short path. 2- update style position and  comment.

--- app/(website)/_components/sections/testimonials.tsx
+++ app/(website)/_components/sections/testimonials.tsx
@@ -17,25 +17,26 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 
-import { testimonials } from "@/app/(website)/constants";
+import { testimonials } from "@website/constants";
 
 export const Testimonials = () => {
   return (
     <section className="max-container section">
       <Carousel plugins={[AutoPlay()]} opts={{ loop: true }}>
         <CarouselContent>
+          {/* TODO: Update data from API */}
           {testimonials.map(({ rate, name, comment, position }, index) => (
             <CarouselItem key={index}>
               <Card className="border-none shadow-none">
                 <CardHeader className="px-0">{rate}</CardHeader>
                 <CardContent className="px-0">
-                  <h3 className="text-2xl leading-6 md:text-3xl md:leading-7 lg:text-4xl lg:leading-9 font-medium  ">
+                  <h3 className="text-2xl leading-8 md:text-3xl md:leading-9 lg:text-4xl lg:leading-[44px] font-medium  ">
                     {comment}
                   </h3>
                 </CardContent>
                 <CardFooter className="px-0 flex flex-col gap-2 items-start">
                   <p>{name}</p>
-                  <p className="text-primary">{position}</p>
+                  <p className="text-[#8082F2]">{position}</p>
                 </CardFooter>
               </Card>
             </CarouselItem>
