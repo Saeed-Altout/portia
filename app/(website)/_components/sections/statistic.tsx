6950fa5e38aadbd9e3904b827b9423190a375832
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-31 16:10:28 +0300

    add border top to card  in small screen

--- app/(website)/_components/sections/statistic.tsx
+++ app/(website)/_components/sections/statistic.tsx
@@ -1,5 +1,5 @@
+import { statisticData } from "@website/constants";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { statisticData } from "@/app/(website)/constants";
 
 export const Statistic = () => {
   return (
@@ -7,17 +7,19 @@ export const Statistic = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* TODO: Update data from API */}
         {statisticData.map(({ value, name, description }, index) => (
-          <Card key={index} className="shadow-none border-none">
-            <CardHeader className="px-0">
-              <CardTitle className="text-primary font-semibold text-5xl lg:text-6xl">
-                {value}
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-1 px-0">
-              <h3 className="font-medium text-lg">{name}</h3>
-              <p>{description}</p>
-            </CardContent>
-          </Card>
+          <div key={index} className="border-t-2 border-primary sm:border-none">
+            <Card className="shadow-none border-none">
+              <CardHeader className="px-0">
+                <CardTitle className="text-primary font-semibold text-5xl lg:text-6xl">
+                  {value}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="space-y-1 px-0">
+                <h3 className="font-medium text-lg">{name}</h3>
+                <p>{description}</p>
+              </CardContent>
+            </Card>
+          </div>
         ))}
       </div>
     </section>
