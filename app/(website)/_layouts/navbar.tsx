53f4b6490bdde835b0b095a979cd3d863042d054
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-03 17:42:35 +0300

    update login button in navbar

--- app/(website)/_layouts/navbar.tsx
+++ app/(website)/_layouts/navbar.tsx
@@ -1,24 +1,26 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { Logo } from "@/components/common/Logo";
-import { NavMain, NavMobile } from "@website/_components/navbar";
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { Logo } from '@/components/common/Logo';
+import { NavMain, NavMobile } from '@website/_components/navbar';
 
 export const Navbar = () => {
-  return (
-    <header className="shadow-md h-20 w-full flex justify-center items-center">
-      <div className="max-container flex justify-between items-center">
-        <Logo />
-        <NavMain />
-        <NavMobile />
-        <div className="hidden lg:flex items-center justify-end gap-x-4">
-          {/* TODO: Update function of this buttons */}
-          <Button variant="ghost" size="sm" className="text-gray-primary ">
-            Log in
-          </Button>
-          <Button>Get Started</Button>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className='shadow-md h-20 w-full flex justify-center items-center'>
+			<div className='max-container flex justify-between items-center'>
+				<Logo />
+				<NavMain />
+				<NavMobile />
+				<div className='hidden lg:flex items-center justify-end gap-x-4'>
+					{/* TODO: Update function of this buttons */}
+					<Button variant='ghost' size='sm' className='text-gray-primary' asChild>
+						<Link href='/auth/login'>Log in</Link>
+					</Button>
+					<Button>Get Started</Button>
+				</div>
+			</div>
+		</header>
+	);
 };
