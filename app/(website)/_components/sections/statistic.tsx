629245f38d02ac13ace66b043f0c3a504499455e
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 15:01:45 +0300

    refactor statistic component.

--- app/(website)/_components/sections/statistic.tsx
+++ app/(website)/_components/sections/statistic.tsx
@@ -1,27 +1,31 @@
-import { statisticData } from "@website/constants";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { statisticData } from '@website/constants';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { Section } from '@website/_components/ui/section';
+import { Container } from '@website/_components/ui/container';
+import { Paragraph } from '@website/_components/ui/paragraph';
 
 export const Statistic = () => {
-  return (
-    <section className="max-container section">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {/* TODO: Update data from API */}
-        {statisticData.map(({ value, name, description }, index) => (
-          <div key={index} className="border-t-2 border-primary sm:border-none">
-            <Card className="shadow-none border-none">
-              <CardHeader className="px-0">
-                <CardTitle className="text-primary font-semibold text-5xl lg:text-6xl">
-                  {value}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-1 px-0">
-                <h3 className="font-medium text-lg">{name}</h3>
-                <p>{description}</p>
-              </CardContent>
-            </Card>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<Section space='wide'>
+			<Container size='wide'>
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 sm:gap-8 w-full'>
+					{statisticData.map(({ value, name, description }, index) => (
+						<Card key={index} className='pt-4 sm:pt-0 border-t-2 border-x-transparent !border-b-transparent border-primary sm:border-none shadow-none rounded-none space-y-3'>
+							<CardHeader className='px-0 p-0'>
+								<CardTitle className='text-primary font-semibold text-5xl lg:text-6xl'>{value}</CardTitle>
+							</CardHeader>
+							<CardContent className='space-y-2 p-0'>
+								<Paragraph size='lg' className='sm:text-black-primary font-medium'>
+									{name}
+								</Paragraph>
+								<Paragraph className='hidden sm:block'>{description}</Paragraph>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</Container>
+		</Section>
+	);
 };
