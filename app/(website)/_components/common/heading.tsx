ae33d29e02112902d5f598568777682b75aaa742
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 15:36:30 +0300

    1- refactor features component. 2- create icon component.

--- app/(website)/_components/common/heading.tsx
+++ app/(website)/_components/common/heading.tsx
@@ -1,36 +1,28 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import * as React from "react";
+import * as React from 'react';
+
+import { Paragraph } from '@website/_components/ui/paragraph';
 
 export const Heading = ({
-  title,
-  description,
-  heading,
-  children,
-  className,
-  titleStyle,
-  descriptionStyle,
+	title,
+	description,
+	heading,
+	children,
 }: {
-  title: string;
-  description?: string;
-  heading?: string;
-  titleStyle?: string;
-  descriptionStyle?: string;
-  children?: React.ReactNode;
+	title: string;
+	description?: string;
+	heading?: string;
+	children?: React.ReactNode;
 } & React.HTMLAttributes<HTMLElement>) => {
-  return (
-    <div className="flex items-start justify-start flex-col gap-5 md:flex-row md:items-center md:justify-between">
-      <div className="space-y-5 flex-1 relative max-w-3xl">
-        <span className="block absolute -top-6 left-0 text-primary font-semibold">
-          {heading}
-        </span>
-        <h1 className={cn("text-3xl md:text-4xl font-semibold", titleStyle)}>
-          {title}
-        </h1>
-        <p className={cn(descriptionStyle)}>{description}</p>
-      </div>
-      <div className="w-full md:w-fit">{children}</div>
-    </div>
-  );
+	return (
+		<div className='flex items-start justify-start flex-col gap-5 md:flex-row md:items-center md:justify-between'>
+			<div className='space-y-4 md:space-y-5 flex-1 relative'>
+				<span className='block absolute -top-6 left-0 text-primary font-semibold'>{heading}</span>
+				<h1 className='text-3xl md:text-4xl font-semibold'>{title}</h1>
+				<Paragraph>{description}</Paragraph>
+			</div>
+			<div className='w-full md:w-fit'>{children}</div>
+		</div>
+	);
 };
--- app/(website)/_components/sections/features.tsx
+++ app/(website)/_components/sections/features.tsx
@@ -1,30 +1,34 @@
-import { featuresData } from "@website/constants";
-import { Heading } from "@website/_components/common/heading";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { featuresData } from '@website/constants';
+import { Heading } from '@website/_components/common/heading';
+
+import { Card, CardContent } from '@/components/ui/card';
+
+import { Icon } from '@website/_components/ui/icon';
+import { Section } from '@website/_components/ui/section';
+import { Container } from '@website/_components/ui/container';
+import { Paragraph } from '@website/_components/ui/paragraph';
 
 export const Features = () => {
-  return (
-    <section className="max-container section space-y-16">
-      <Heading
-        title="What makes Portia.io Different?"
-        description="We provide a lots of incredible features that you will need!"
-      />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {/* TODO: Update data from API */}
-        {featuresData.map(({ icon: Icon, title, description }, index) => (
-          <Card key={index} className="shadow-none border-none">
-            <CardHeader className="px-0">
-              <CardTitle className="bg-[#B5B6F7] border-[8px] border-[#D4D4FF] w-[48px] h-[48px] flex items-center justify-center rounded-full">
-                <Icon className="text-primary h-5 w-5" />
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-[6px] px-0">
-              <h3 className="font-medium text-[20px] capitalize">{title}</h3>
-              <p>{description}</p>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<Section space='wide'>
+			<Container size='wide' className='gap-y-12 md:gap-y-16'>
+				<Heading title='What makes Portia.io Different?' description='We provide a lots of incredible features that you will need!' />
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16'>
+					{featuresData.map(({ icon: LIcon, title, description }, index) => (
+						<Card key={index} className='shadow-none border-none space-y-5'>
+							<Icon>
+								<LIcon className='text-primary h-5 w-5' />
+							</Icon>
+							<CardContent className='space-y-2 p-0'>
+								<Paragraph size='lg' className='font-medium text-black-primary capitalize'>
+									{title}
+								</Paragraph>
+								<Paragraph>{description}</Paragraph>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</Container>
+		</Section>
+	);
 };
