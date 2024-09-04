4a0ef4a8dcbe1960c0031baddf596b06940c8527
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 16:05:01 +0300

    refactor faqs component.

--- app/(website)/_components/sections/faqs.tsx
+++ app/(website)/_components/sections/faqs.tsx
@@ -1,50 +1,45 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
+import { faqsData } from '@website/constants';
 
-import { Heading } from "@website/_components/common/heading";
-import { faqsData } from "@website/constants";
+import { Button } from '@/components/ui/button';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+
+import { Heading } from '@website/_components/common/heading';
+
+import { Section } from '@website/_components/ui/section';
+import { Container } from '@website/_components/ui/container';
+import { Paragraph } from '@website/_components/ui/paragraph';
 
 export const FAQs = () => {
-  return (
-    <section className="max-container section">
-      <div className="flex items-start justify-between flex-col lg:flex-row gap-8">
-        <div className="max-w-[448px]">
-          <Heading title="FAQs" />
-          <p className="text-gray-primary text-sm md:text-base">
-            Here are the most common questions and how we answer them,
-            Can&apos;t find the answer you&apos;re looking for? Please{" "}
-            <Button variant="link" asChild className="px-0 underline" size="sm">
-              {/* TODO: Update href link of sales team */}
-              <Link href="/" className="!text-gray-primary">
-                chat to Sales team.
-              </Link>
-            </Button>
-          </p>
-        </div>
+	return (
+		<Section space='wide'>
+			<Container size='wide' view='vertical' className='gap-x-8 gap-y-12 lg:flex-row'>
+				<div className='max-w-[448px]'>
+					<Heading title='FAQs' />
+					<Paragraph size='lg'>
+						Here are the most common questions and how we answer them, Can&apos;t find the answer you&apos;re looking for? Please{' '}
+						<Button variant='link' asChild className='p-0 underline' size='sm'>
+							<Link href='/' className='!text-gray-primary'>
+								chat to Sales team.
+							</Link>
+						</Button>
+					</Paragraph>
+				</div>
 
-        <div className="flex-1 w-full">
-          <Accordion type="single" collapsible className="flex flex-col gap-5">
-            {/* TODO: Update data from API */}
-            {faqsData.map(({ question, answer }, index) => (
-              <AccordionItem key={index} value={`item-${index + 1}`}>
-                <AccordionTrigger>
-                  <span className="font-medium text-lg">{question}</span>
-                </AccordionTrigger>
-                <AccordionContent className="text-gray-primary text-sm md:text-base">
-                  {answer}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </div>
-      </div>
-    </section>
-  );
+				<div className='flex-1 w-full'>
+					<Accordion type='single' collapsible className='flex flex-col gap-5'>
+						{faqsData.map(({ question, answer }, index) => (
+							<AccordionItem key={index} value={`item-${index + 1}`}>
+								<AccordionTrigger>
+									<span className='font-medium text-lg text-left'>{question}</span>
+								</AccordionTrigger>
+								<AccordionContent className='text-gray-primary text-base'>{answer}</AccordionContent>
+							</AccordionItem>
+						))}
+					</Accordion>
+				</div>
+			</Container>
+		</Section>
+	);
 };
