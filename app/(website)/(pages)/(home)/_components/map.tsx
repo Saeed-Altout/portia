b6c54c043bb6854b9f99a33a2d4239849da055d3
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 18:01:15 +0300

    refactor map component.

--- app/(website)/(pages)/(home)/_components/map.tsx
+++ app/(website)/(pages)/(home)/_components/map.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image';
+
+import { Container } from '@website/_components/ui/container';
+import { Section } from '@website/_components/ui/section';
 
 export const Map = () => {
-  return (
-    <section className="max-container">
-      <div className="relative h-[250px] md:h-[450px] lg:h-[800px]">
-        <Image
-          src="/icons/map.svg"
-          alt="Map"
-          className="w-full h-full object-contain"
-          fill
-        />
-      </div>
-    </section>
-  );
+	return (
+		<Section space='wide'>
+			<Container size='wide'>
+				<div className='relative h-[250px] md:h-[450px] lg:h-[800px]'>
+					<Image src='/icons/map.svg' alt='Map' className='w-full h-full object-contain' fill />
+				</div>
+			</Container>
+		</Section>
+	);
 };
