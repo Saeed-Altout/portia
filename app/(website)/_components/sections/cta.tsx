efbaa1a29855fc112469dc52995749ab9b260834
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 16:10:39 +0300

    refactor CTA component.

--- app/(website)/_components/sections/cta.tsx
+++ app/(website)/_components/sections/cta.tsx
@@ -1,23 +1,23 @@
-import { Button } from "@/components/ui/button";
-import { Heading } from "@website/_components/common/heading";
+import { Button } from '@/components/ui/button';
 
-import React from "react";
+import { Heading } from '@website/_components/common/heading';
+
+import { Section } from '@website/_components/ui/section';
+import { Container } from '@website/_components/ui/container';
 
 export const CTA = () => {
-  return (
-    <section className="max-container section">
-      <Heading
-        title="Ready To Take Your Online Efficiency to the Next Level ?"
-        description="Join over 40,000+ happy clients already growing with portia.io ."
-        descriptionStyle="lg:text-xl lg:p-2"
-      >
-        <div className="flex items-center justify-center flex-col md:flex-row gap-5">
-          <Button variant="outline" className="w-full">
-            Contact us
-          </Button>
-          <Button className="w-full -order-1 md:order-1">Get Started</Button>
-        </div>
-      </Heading>
-    </section>
-  );
+	return (
+		<Section space='wide'>
+			<Container size='wide'>
+				<Heading title='Ready To Take Your Online Efficiency to the Next Level ?' description='Join over 40,000+ happy clients already growing with portia.io .'>
+					<div className='flex items-center justify-center flex-col md:flex-row gap-3'>
+						<Button variant='outline' className='w-full'>
+							Contact us
+						</Button>
+						<Button className='w-full -order-1 md:order-1'>Get Started</Button>
+					</div>
+				</Heading>
+			</Container>
+		</Section>
+	);
 };
