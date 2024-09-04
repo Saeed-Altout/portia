5cdfe746c81952723fe26534a9c89b1ccfa74728
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 18:23:50 +0300

    complete refactor contact us page.

--- app/(website)/(pages)/contact-us/page.tsx
+++ app/(website)/(pages)/contact-us/page.tsx
@@ -1,20 +1,23 @@
-import { Statistic } from "@website/_components/sections";
-import { Heading } from "@website/_components/common";
-import { ContactForm } from "./_components";
+import { ContactForm } from './_components';
+
+import { Heading } from '@website/_components/common';
+import { Statistic } from '@website/_components/sections';
+
+import { Section } from '@website/_components/ui/section';
+import { Container } from '@website/_components/ui/container';
 
 export default function ContactUsPage() {
-  return (
-    <main>
-      <section className="max-container section">
-        <Heading
-          title="Get in touch"
-          description="Our friendly team would love to hear from you."
-        />
-        <ContactForm />
-      </section>
-      <div className="hidden lg:block">
-        <Statistic />
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<Section className='pt-16 pb-24'>
+				<Container size='wide' className='gap-y-12'>
+					<Heading title='Get in touch' description='Our friendly team would love to hear from you.' />
+					<ContactForm />
+				</Container>
+			</Section>
+			<div className='hidden lg:block'>
+				<Statistic />
+			</div>
+		</main>
+	);
 }
---
+++ app/(website)/schema/index.ts
@@ -0,0 +1,21 @@
+import * as z from 'zod';
+
+export const formContactSchema = z.object({
+	firstName: z.string().min(2, {
+		message: 'First Name must be at least 2 characters.',
+	}),
+	lastName: z.string().min(2, {
+		message: 'Last Name must be at least 2 characters.',
+	}),
+	email: z.string().email(),
+	phoneNumber: z.object({
+		country: z.string().min(2, { message: 'Country is required!' }).default('syria'),
+		number: z.string().min(9, { message: 'Phone number must be at least 9 numbers.' }),
+	}),
+	message: z.string().optional(),
+	agreeToPrivacyPolicy: z.boolean().refine((val) => val === true, {
+		message: 'You must agree to our friendly privacy policy.',
+	}),
+});
+
+export type FormContactValues = z.infer<typeof formContactSchema>;
