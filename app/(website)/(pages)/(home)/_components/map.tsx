3e0be2892ba4549eb3ca31c4d566a61954545c6d
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-04 18:26:30 +0300

    fix "map not found"

--- app/(website)/(pages)/(home)/_components/map.tsx
+++ app/(website)/(pages)/(home)/_components/map.tsx
@@ -6,10 +6,8 @@ import { Section } from '@website/_components/ui/section';
 export const Map = () => {
 	return (
 		<Section space='wide'>
-			<Container size='wide'>
-				<div className='relative h-[250px] md:h-[450px] lg:h-[800px]'>
-					<Image src='/icons/map.svg' alt='Map' className='w-full h-full object-contain' fill />
-				</div>
+			<Container size='wide' className='relative h-[250px] md:h-[450px] lg:h-[800px]'>
+				<Image src='/icons/map.svg' alt='Map' className='w-full h-full object-contain' fill />
 			</Container>
 		</Section>
 	);
