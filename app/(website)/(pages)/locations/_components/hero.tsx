07138044255caa8eb66e3de4ab14c7ef9e0283ee
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-11 10:37:02 +0300

    create hero of locations page.

---
+++ app/(website)/(pages)/locations/_components/hero.tsx
@@ -0,0 +1,17 @@
+import { Section } from "@website/_components/ui/section";
+import { Container } from "@website/_components/ui/container";
+import { HeadingPage } from "@website/_components/ui/heading-page";
+
+export const Hero = () => {
+  return (
+    <Section>
+      <Container>
+        <HeadingPage
+          label="Available Locations"
+          title="Look at our various providers & Locations"
+          description="We have a lot of proxy’s location that attach with you needs."
+        />
+      </Container>
+    </Section>
+  );
+};
