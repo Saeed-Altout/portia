e878b74ed52ed9bc33817e3c4ad06ed8bd1c1602
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-23 12:50:09 +0300

    refactor navbar

--- app/dashboard/_layouts/navbar.tsx
+++ app/dashboard/_layouts/navbar.tsx
@@ -1,55 +1,39 @@
 "use client";
 
+import * as React from "react";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import { Bell, Menu, Settings } from "lucide-react";
-
-import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
+import { routes } from "@dashboard/constants";
+
 export const Navbar = () => {
   const pathname = usePathname();
 
-  const routes = [
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-      active: pathname === "/dashboard",
-    },
-    {
-      label: "My Proxies",
-      href: "/dashboard/my-proxies",
-      active: pathname === "/dashboard/my-proxies",
-    },
-    {
-      label: "Pricing Plans",
-      href: "/dashboard/pricing-plans",
-      active: pathname === "/dashboard/pricing-plans",
-    },
-    {
-      label: "Deposits",
-      href: "/dashboard/deposits",
-      active: pathname === "/dashboard/deposits",
-    },
-  ];
-
   return (
     <header className="sticky top-0 flex h-[72px] items-center gap-4 bg-background px-4 md:px-6 z-10">
-      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      {/* Main navbar  */}
+      <nav className="hidden md:flex flex-row items-center gap-6">
+        {/* Logo */}
         <Link href="/dashboard" className="relative h-[27px] w-[91px]">
           <Image src="/icons/logo.svg" alt="Logo" fill />
         </Link>
+        {/* Routes */}
         <div className="flex justify-start items-center gap-x-2">
-          {routes.map(({ label, href, active }, index) => (
+          {routes.map(({ label, href }, index) => (
             <Button
               key={index}
               variant="ghost"
-              className={cn(active && "bg-secondary transition-all")}
+              size="sm"
+              className={cn(href === pathname && "bg-secondary transition-all")}
               asChild
             >
               <Link key={index} href={href}>
@@ -59,25 +43,29 @@ export const Navbar = () => {
           ))}
         </div>
       </nav>
+      {/* Mobile navbar */}
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0 md:hidden">
             <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
+            <span className="sr-only">Toggle menu</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="right" className="overflow-y-auto">
+          {/* Logo */}
           <Link href="/dashboard" className="relative h-[27px] w-[91px] block">
             <Image src="/icons/logo.svg" alt="Logo" fill />
           </Link>
-          <div className="w-full mt-10 flex flex-col justify-start items-center gap-y-4">
-            {routes.map(({ label, href, active }, index) => (
+          {/* Routes */}
+          <div className="w-full mt-10 flex flex-col justify-start items-center gap-y-5">
+            {routes.map(({ label, href }, index) => (
               <Button
                 key={index}
                 variant="ghost"
+                size="sm"
                 className={cn(
                   "w-full",
-                  active && "bg-secondary transition-all"
+                  href === pathname && "bg-secondary transition-all"
                 )}
                 asChild
               >
@@ -89,17 +77,19 @@ export const Navbar = () => {
           </div>
         </SheetContent>
       </Sheet>
+
+      {/* Icons navbar */}
       <div className="flex items-center gap-x-2 w-fit ml-auto">
         <Button size="icon" variant="ghost" asChild>
           <Link href="/dashboard/settings">
             <Settings className="h-4 w-4 text-gray-primary" />
-            <span className="sr-only">Settings</span>
+            <span className="sr-only">Settings icon</span>
           </Link>
         </Button>
         <Button size="icon" variant="ghost" asChild>
           <Link href="/dashboard/notifications">
             <Bell className="h-4 w-4 text-gray-primary" />
-            <span className="sr-only">Bell</span>
+            <span className="sr-only">Bell icon</span>
           </Link>
         </Button>
       </div>
---
+++ app/dashboard/constants/index.ts
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    label: "My Proxies",
+    href: "/dashboard/my-proxies",
+  },
+  {
+    label: "Pricing Plans",
+    href: "/dashboard/pricing-plans",
+  },
+  {
+    label: "Deposits",
+    href: "/dashboard/deposits",
+  },
+];
