74b3782af5cc12cd0a2fb6004b740fbd09d01b8a
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-23 16:53:48 +0300

    refactor code

--- app/dashboard/_components/modals/renew-expired-proxies-modal.tsx
+++ app/dashboard/_components/modals/renew-expired-proxies-modal.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import * as React from "react";
+
 import { BeatLoader } from "react-spinners";
-import { Zap } from "lucide-react";
+import { Key, User, Zap } from "lucide-react";
 import { toast } from "sonner";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
-import { StepOne } from "../steps-expired-proxies/stepOne";
-import { StepTwo } from "../steps-expired-proxies/stepTwo";
-
 import { Modal } from "@dashboard/_components/ui/modal";
+import { CustomField, FiledType } from "@dashboard/_components/ui/custom-field";
 
 const activateNewProxySchema = z.object({
   package: z.string().min(2),
@@ -149,5 +148,107 @@ export const RenewExpiredProxiesModal = ({
         </form>
       </Form>
     </Modal>
+  );
+};
+
+interface StepOneProps {
+  isLoading?: boolean;
+}
+
+export const StepOne = ({ isLoading }: StepOneProps) => {
+  const planData = [
+    { value: "hourly", label: "Hourly" },
+    { value: "monthly", label: "Monthly" },
+    { value: "yearly", label: "Yearly" },
+  ];
+
+  const amountData = [
+    { value: "10h", label: "10 h" },
+    { value: "20h", label: "20 h" },
+    { value: "30h", label: "30 h" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-between items-center gap-x-4">
+        <CustomField
+          name="plan"
+          label="Plan"
+          placeholder="Select a plan"
+          className="w-2/3"
+          type={FiledType.SELECT}
+          options={planData}
+          isLoading={isLoading}
+        />
+        <CustomField
+          name="amount"
+          label="Amount"
+          placeholder="Amount"
+          className="w-1/3"
+          type={FiledType.SELECT}
+          options={amountData}
+          isLoading={isLoading}
+        />
+      </div>
+      <CustomField
+        name="provider"
+        label="Provider & Location"
+        placeholder="Provider & Location"
+        type={FiledType.TEXT}
+        href="/dashboard/my-proxies"
+        isLoading={true}
+      />
+      <CustomField
+        name="ipRotation"
+        label="Minimum time between IP rotation"
+        placeholder="IP rotation"
+        type={FiledType.TEXT}
+        isLoading={isLoading}
+      />
+    </>
+  );
+};
+
+interface StepTwoProps {
+  isLoading?: boolean;
+}
+
+export const StepTwo = ({ isLoading }: StepTwoProps) => {
+  const proxyTypeData = [
+    { value: "http-proxy", label: "Http proxy" },
+    { value: "https-proxy", label: "Https proxy" },
+  ];
+  return (
+    <>
+      <CustomField
+        name="proxyType"
+        label="Proxy Type"
+        placeholder="Proxy type"
+        type={FiledType.SELECT}
+        options={proxyTypeData}
+        isLoading={isLoading}
+      />
+      <CustomField
+        name="autoRenew"
+        labelCheckbox="Auto Renew"
+        type={FiledType.CHECKBOX}
+        isLoading={isLoading}
+      />
+      <CustomField
+        name="username"
+        label="Proxy Authentications"
+        placeholder="Username"
+        icon={User}
+        type={FiledType.TEXT}
+        isLoading={isLoading}
+      />
+      <CustomField
+        name="password"
+        placeholder="Password"
+        icon={Key}
+        type={FiledType.TEXT}
+        isLoading={isLoading}
+      />
+    </>
   );
 };
