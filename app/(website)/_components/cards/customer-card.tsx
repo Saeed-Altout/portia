2269519ec6b771bef7c377b37274cb042739c521
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-25 11:24:48 +0300

    add animate to card testimonials

--- app/(website)/_components/cards/customer-card.tsx
+++ app/(website)/_components/cards/customer-card.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import Image from "next/image";
-
+import { motion } from "framer-motion";
 import { FaStar } from "react-icons/fa";
 
 interface CustomersCardProps {
@@ -19,27 +19,55 @@ export const CustomersCard = ({
 }: {
   initialData: CustomersCardProps;
 }) => {
+  const [isShowComment, setIsShowComment] = React.useState<boolean>(false);
+
   const { imgUrl, agency, rate, comment, name, position } = initialData;
+
+  const fadeTransition = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.3 },
+  };
+
   return (
-    <div>
+    <div
+      onMouseEnter={() => setIsShowComment(true)}
+      onMouseLeave={() => setIsShowComment(false)}
+    >
       <Image src={imgUrl} alt={agency} fill className="object-cover" />
-      <div className="absolute bottom-0 drop-shadow-xl left-0 w-full min-h-[202px] duration-300 ease-in-out bg-white bg-opacity-10 backdrop-blur-lg flex items-center justify-start">
+      <motion.div
+        initial={{ height: 212 }}
+        animate={{ height: isShowComment ? 260 : 212 }}
+        transition={{ duration: 0.3 }}
+        className="absolute bottom-0 drop-shadow-xl left-0 w-full min-h-[212px] bg-white bg-opacity-10 backdrop-blur-lg flex items-end justify-start"
+      >
         <div className="text-left p-6 pb-8 space-y-1">
-          <div className="flex items-center justify-start gap-x-1 group-hover:hidden py-4">
-            {[...Array(rate)].map((_, index) => (
-              <FaStar key={index} className="text-white h-4 w-4" />
-            ))}
-          </div>
-          <p className="text !text-white !text-lg hidden group-hover:block py-4">
-            {comment}
-          </p>
+          {!isShowComment && (
+            <motion.div
+              {...fadeTransition}
+              className="flex items-center justify-start gap-x-1 py-4"
+            >
+              {[...Array(rate)].map((_, index) => (
+                <FaStar key={index} className="text-white h-4 w-4" />
+              ))}
+            </motion.div>
+          )}
+          {isShowComment && (
+            <motion.p
+              {...fadeTransition}
+              className="text !text-white !text-lg py-4"
+            >
+              {comment}
+            </motion.p>
+          )}
           <p className="text !text-white !text-3xl !font-semibold">{name}</p>
           <p className="text !text-white !text-lg !font-medium pt-2">
             {position}
           </p>
           <p className="text !text-white">{agency}</p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
