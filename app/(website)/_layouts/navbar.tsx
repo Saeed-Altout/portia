37fc86f9b6b6f6b8d8545881bfb061f3373a253c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-09-27 15:15:26 +0300

    remove button login if authenticated

--- app/(website)/_layouts/navbar.tsx
+++ app/(website)/_layouts/navbar.tsx
@@ -9,9 +9,11 @@ import { Container } from "@website/_components/ui/container";
 import { NavMain, NavMobile } from "@website/_components/navbar";
 
 import { useScrollStore } from "@website/hooks/scroll-to";
+import { useAuth } from "@/providers/auth-provider";
 
 export const Navbar = () => {
   const scrollStore = useScrollStore();
+  const { isAuthenticated } = useAuth();
 
   return (
     <header className="shadow-md h-20 w-full flex justify-center items-center">
@@ -20,9 +22,11 @@ export const Navbar = () => {
         <NavMain />
         <NavMobile />
         <div className="hidden lg:flex items-center justify-end gap-x-4">
-          <Button variant="ghost" className="text-gray-500" asChild>
-            <Link href="/auth/login">Log in</Link>
-          </Button>
+          {!isAuthenticated && (
+            <Button variant="ghost" className="text-gray-500" asChild>
+              <Link href="/auth/login">Log in</Link>
+            </Button>
+          )}
           <Button onClick={scrollStore.scrollToSection}>Get Started</Button>
         </div>
       </Container>
