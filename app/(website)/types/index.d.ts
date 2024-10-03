ac33302c308da14649faa7ab711c2aaaa3fe007c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-03 17:54:27 +0300

    update code

--- app/(website)/types/index.d.ts
+++ app/(website)/types/index.d.ts
@@ -23,4 +23,47 @@ declare type Faq = {
 	created_at: Date | string;
 	updated_at: Date | string;
 };
-declare type Offer = {};
+
+declare type Plan = {
+	id: number;
+	name: string;
+};
+
+declare type Feature = {
+	id: number;
+	feature_group_id: number;
+	name: string;
+	description: string | null;
+	value: string | boolean | null;
+	created_at: Date | string;
+	updated_at: Date | string;
+};
+
+declare type FeatureGroup = {
+	id: number;
+	name: string;
+	package_id: number;
+	features: Feature[];
+	created_at: Date | string;
+	updated_at: Date | string;
+};
+
+declare type Package = {
+	id: number;
+	name: string;
+	description: string | null;
+	created_at: Date | string;
+	updated_at: Date | string;
+	feature_groups: FeatureGroup[];
+};
+
+declare type Offer = {
+	id: number;
+	amount: number;
+	cost: string;
+	is_top: boolean;
+	plan: Plan;
+	package: Package;
+	created_at: Date | string;
+	updated_at: Date | string;
+};
