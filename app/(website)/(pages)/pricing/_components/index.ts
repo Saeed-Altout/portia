c2ef4d3c94ac7e4f6822310e46e31adb1d4f55fd
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-04 22:46:35 +0300

    update code

--- app/(website)/(pages)/pricing/_components/index.ts
+++ app/(website)/(pages)/pricing/_components/index.ts
@@ -1,6 +1,4 @@
-import { Hero } from "./hero";
-import { Content } from "./content";
-import { TableMain } from "./table-main";
-import { TableMobile } from "./table-mobile";
+import { Hero } from './hero';
+import { Content } from './content';
 
-export { Hero, Content, TableMain, TableMobile };
+export { Hero, Content };
