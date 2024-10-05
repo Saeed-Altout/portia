09b56c04ce0219ff0de436dfa2ed6ccd7566d3f6
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-05 23:52:56 +0300

    create table response and ready to link

--- app/(website)/(pages)/pricing/_components/content.tsx
+++ app/(website)/(pages)/pricing/_components/content.tsx
@@ -1,86 +1,69 @@
-import { Section } from '@website/_components/ui/section';
-import { Container } from '@website/_components/ui/container';
-import { Button } from '@/components/ui/button';
+import * as React from 'react';
 import Link from 'next/link';
+
+import { data } from '@website/constants';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+
+import { Container } from '@website/_components/ui/container';
+import { Section } from '@website/_components/ui/section';
 
 export const Content = () => {
 	return (
 		<Section>
 			<Container>
-				<div className='offer-table'>
-					<div className='offer-table-column'>
-						<div className='offer-table-head'>
-							<div className='offer-table-head-info'>
-								<h3 className='offer-table-head-info-name'>Basic</h3>
-								{true && <span className='offer-table-head-info-popular'>Popular</span>}
+				<div className='w-full grid grid-cols-1 md:grid-cols-4'>
+					{data.map((offer, offerIndex) => (
+						<div key={offerIndex} className={cn(offerIndex === 0 && 'col-span-1 md:col-span-2')}>
+							<div className={cn('offer-table-head', offerIndex === 0 && 'md:w-[50%] md:ml-auto')}>
+								<div className='offer-table-head-info'>
+									<h3 className='offer-table-head-info-name'>{offer.package?.name}</h3>
+									{true && <span className='offer-table-head-info-popular'>Popular</span>}
+								</div>
+								<div className='offer-table-head-content'>
+									<h3 className='offer-table-head-content-price'>
+										{offer.cost}
+										<span className='offer-table-head-content-plan'>{offer.plan?.name}</span>
+									</h3>
+									<p className='offer-table-head-content-description'>{offer.package?.description}</p>
+									<Button className='w-full' asChild>
+										<Link href='/'>Get started</Link>
+									</Button>
+								</div>
 							</div>
-							<div className='offer-table-head-content'>
-								<h3 className='offer-table-head-content-price'>
-									20
-									<span className='offer-table-head-content-plan'>per month</span>
-								</h3>
-								<p className='offer-table-head-content-description'>
-									Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing
-								</p>
-								<Button className='w-full' asChild>
-									<Link href='/'>Get started</Link>
-								</Button>
-							</div>
-						</div>
-						<div className='offer-table-body'>
-							<ul className='offer-table-group-features'>
-								<li className='offer-table-cell-data' />
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-							</ul>
-							<ul className='offer-table-group-features'>
-								<li className='offer-table-cell-data' />
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-							</ul>
-							<ul className='offer-table-group-features'>
-								<li className='offer-table-cell-data' />
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-								<li className='offer-table-cell-data'>feature1</li>
-							</ul>
-						</div>
-						<div className='table-footer py-6 px-4'>
-							<div className='flex flex-col gap-y-3'>
-								<Button className='w-full'>Get Started</Button>
-								<Button className='w-full' variant='outline'>
-									Contact Sales
-								</Button>
+							<div>
+								{offer.package?.feature_groups?.map((featureGroup, groupIndex) => (
+									<React.Fragment key={groupIndex}>
+										<ul className='flex flex-col border-b'>
+											{featureGroup.features?.map((feature, featureIndex) => (
+												<li
+													className={cn(
+														'cell relative feature-item',
+														offerIndex !== 0 && 'before:md:hidden'
+													)}
+													data-content={feature.name}
+													key={featureIndex}
+												>
+													<p className={cn(offerIndex === 0 && 'md:w-1/2 md:ml-auto')}>
+														{typeof feature.value === 'string' ? (
+															feature.value
+														) : feature.value === true ? (
+															<span className='cell-check'>✔</span>
+														) : (
+															<span>━</span>
+														)}
+													</p>
+												</li>
+											))}
+										</ul>
+									</React.Fragment>
+								))}
 							</div>
 						</div>
-					</div>
+					))}
 				</div>
 			</Container>
 		</Section>
 	);
 };
-
-{
-	/* <td key={index} className="px-6 py-5 text-center">
-{plan.overview.basic_features ? (
-  <span className="text-[#26A6A4] bg-[#B5F7F6] block rounded-full w-6 h-6 mx-auto p-[2px]">
-    ✔
-  </span>
-) : (
-  "━"
-)}
-</td> */
-}
-
-{
-	/* <td key={index} className="px-6 pb-4 pt-12" /> */
-}
-
-// <td key={index} className="px-6 py-5 text-center text-gray-500">
-// {plan.overview.users}
-// </td>
