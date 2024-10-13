43750778a8a644b4e6c35eeb33895a7e02f60836
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-13 10:53:33 +0300

    update code

--- app/dashboard/(pages)/locations/_components/cell-actions.tsx
+++ app/dashboard/(pages)/locations/_components/cell-actions.tsx
@@ -1,8 +1,5 @@
-import * as React from "react";
-import { Info } from "lucide-react";
-
 import { cn } from "@/lib/utils";
-
+import { Info } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 interface CellActionsProps {
