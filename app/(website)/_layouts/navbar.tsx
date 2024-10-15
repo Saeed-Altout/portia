c8a82b1404c2970cd0f972d5fa44d4aebe12ae17
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-15 13:46:49 +0300

    fix code

--- app/(website)/_layouts/navbar.tsx
+++ app/(website)/_layouts/navbar.tsx
@@ -9,17 +9,13 @@ import { Container } from "@website/_components/ui/container";
 import { NavMain, NavMobile } from "@website/_components/navbar";
 
 import { useScrollStore } from "@website/hooks/scroll-to";
-import { LogOut } from "lucide-react";
-import { useLogout } from "@/app/auth/hooks";
 
 export const Navbar = () => {
   const scrollStore = useScrollStore();
-  const { onSubmit, isPending } = useLogout();
 
   return (
     <header className="shadow-md h-20 w-full flex justify-center items-center">
       <Container className="w-full flex-row justify-between items-center">
-        <Logo />
         <NavMain />
         <NavMobile />
         <div className="hidden lg:flex items-center justify-end gap-x-4">
@@ -27,9 +23,7 @@ export const Navbar = () => {
             <Link href="/auth/login">Log in</Link>
           </Button>
 
-          <Button disabled={isPending} onClick={scrollStore.scrollToSection}>
-            Get Started
-          </Button>
+          <Button onClick={scrollStore.scrollToSection}>Get Started</Button>
         </div>
       </Container>
     </header>
