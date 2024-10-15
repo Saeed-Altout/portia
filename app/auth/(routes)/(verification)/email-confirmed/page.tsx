de22f8aa0ffd92f31ca2a7571875b2467b8c0ae7
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-15 14:12:31 +0300

    fix error

--- app/auth/(routes)/(verification)/email-confirmed/page.tsx
+++ app/auth/(routes)/(verification)/email-confirmed/page.tsx
@@ -1,57 +1,12 @@
-"use client";
+import { Suspense } from "react";
 
-import Link from "next/link";
-import { useEffect } from "react";
-import { CheckCircle } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Circle, Icon } from "@/components/circle-icon";
-
-import { BackButton } from "@auth/_components";
-
-import cookieStorage from "@/services/cookie-storage";
+import { Loader } from "@/components/ui/loader";
+import EmailConfirmedForm from "@/app/auth/_components/email-confirmed-form";
 
 export default function EmailConfirmedPage() {
-  const params = useSearchParams();
-  const token = params.get("token");
-
-  useEffect(() => {
-    if (token) {
-      cookieStorage.setAccessToken(token);
-    }
-  }, [token]);
-
   return (
-    <Card className="w-full max-w-[360px] border-none shadow-none pt-24">
-      <CardHeader className="flex flex-col items-center justify-center gap-y-3">
-        <Circle size="lg" fill="success">
-          <Icon size="lg" theme="success" icon={CheckCircle} />
-        </Circle>
-        <CardTitle className="text-2xl md:text-3xl font-semibold text-center">
-          Email verified
-        </CardTitle>
-        <CardDescription className="text-center">
-          Your password has been successfully reset. Click below to log in
-          magically.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Button className="w-full" asChild>
-          <Link href="/dashboard">Continue to dashboard</Link>
-        </Button>
-      </CardContent>
-      <CardFooter>
-        <BackButton label="Back to home" href="/" />
-      </CardFooter>
-    </Card>
+    <Suspense fallback={<Loader />}>
+      <EmailConfirmedForm />
+    </Suspense>
   );
 }
