fc136ce8c9a3662bdd611c69412a3d04c1022cef
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-15 16:21:36 +0300

    fix error

--- app/(website)/(pages)/locations/_components/map.tsx
+++ app/(website)/(pages)/locations/_components/map.tsx
@@ -5,7 +5,7 @@ import { Section } from "@website/_components/ui/section";
 
 export const Map = () => {
   return (
-    <Section className="pt-0">
+    <Section className="pt-0 px-10">
       <Container className="relative h-[250px] md:h-[450px] lg:h-[800px]">
         <Image
           src="/icons/map2.svg"
