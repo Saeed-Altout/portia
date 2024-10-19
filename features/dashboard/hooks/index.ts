c4ffedbadd87bda4736ef4063fd78a03a022ec1c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-20 00:24:17 +0300

    update code

--- features/dashboard/hooks/index.ts
+++ features/dashboard/hooks/index.ts
@@ -2,7 +2,7 @@ import { useResponse } from "./use-response";
 import { useGetUserDetails } from "./use-get-user-details";
 import { useUpdateUserProfile } from "./use-update-user-profile";
 import { useGetPricingPlans } from "./use-get-pricing-plans";
-import { useGetAffiliateEarnings } from "./get-affiliate-earnings";
+import { useGetAffiliateEarnings } from "./use-get-affiliate-earnings";
 export {
   useResponse,
   useGetUserDetails,
