34bd372f90375a49acc30791cbe8bf9558273079
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-27 20:14:37 +0300

    complete refactor contact us page.

--- app/(root)/(routes)/contact-us/_components/contact-form.tsx
+++ app/(root)/(routes)/contact-us/_components/contact-form.tsx
@@ -7,8 +7,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { BeatLoader } from "react-spinners";
 import { PhoneInput } from "react-international-phone";
 
-import { Section } from "@/components/root/ui/section";
-import { Container } from "@/components/root/ui/container";
 import { HeadingPage } from "@/components/root/ui/heading-page";
 
 import { Input } from "@/components/ui/input";
@@ -34,89 +32,87 @@ export const ContactForm = () => {
   const onSubmit = async (values: z.infer<typeof formContactSchema>) => mutate(values);
 
   return (
-    <Section className="pt-16 pb-24">
-      <Container className="gap-y-6">
-        <HeadingPage title="Get in touch" description="Our friendly team would love to hear from you." />
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
-              <FormField
-                control={form.control}
-                name="first_name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>First Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="first name" disabled={isPending} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="last_name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Last Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="last name" disabled={isPending} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input type="email" placeholder="email" disabled={isPending} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="phone"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Phone number</FormLabel>
-                    <FormControl>
-                      <PhoneInput
-                        disabled={isPending}
-                        className="react-international-phone-input-container"
-                        defaultCountry="sy"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="message"
-                render={({ field }) => (
-                  <FormItem className="col-span-1 md:col-span-2">
-                    <FormLabel>Message</FormLabel>
-                    <FormControl>
-                      <Textarea rows={6} placeholder="write here your message" disabled={isPending} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <Button type="submit" disabled={isPending}>
-              {isPending ? <BeatLoader color="#fff" size={12} /> : "Send message"}
-            </Button>
-          </form>
-        </Form>
-      </Container>
-    </Section>
+    <section id="contact-form" className="screen pt-16 pb-24 space-y-12">
+      <HeadingPage title="Get in touch" description="Our friendly team would love to hear from you." />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <FormField
+              control={form.control}
+              name="first_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="first name" disabled={isPending} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="last_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="last name" disabled={isPending} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="email" disabled={isPending} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone number</FormLabel>
+                  <FormControl>
+                    <PhoneInput
+                      disabled={isPending}
+                      className="react-international-phone-input-container"
+                      defaultCountry="sy"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem className="col-span-1 md:col-span-2">
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Textarea rows={6} placeholder="write here your message" disabled={isPending} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={isPending}>
+            Send message {isPending && <BeatLoader color="#fff" size={12} className="ml-2" />}
+          </Button>
+        </form>
+      </Form>
+    </section>
   );
 };
