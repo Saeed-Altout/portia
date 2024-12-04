5df7d5d7b91082ba54cd74da6c3c35e4eb498913
Saeed Altout <saeedaltoutpro@gmail.com>
2024-12-04 23:25:01 +0300

    create loading page

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,9 +1,8 @@
-import { BounceLoader } from "react-spinners";
-
 export const Loader = () => {
   return (
-    <div className="fixed w-full h-full inset-0 z-50 bg-white/80 flex justify-center items-center">
-      <BounceLoader color="#111280" size={70} />
+    <div className="z-50 absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-50 backdrop-blur-sm flex-col gap-7">
+      <div className="w-16 h-16 border-[6px] border-[#D4D4FF] border-l-[#111280] rounded-full animate-spin"></div>
+      <p className="text-[#24242B] font-medium text-xl">Loading...</p>
     </div>
   );
 };
