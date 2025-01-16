6dee5fe44c29990ba631fc49c85008a3eea7f0f6
Saeed Altout <saeedaltoutpro@gmail.com>
2025-01-16 13:27:27 +0300

    add animate to hero

--- app/(root)/(routes)/(home)/_components/hero.tsx
+++ app/(root)/(routes)/(home)/_components/hero.tsx
@@ -1,13 +1,25 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
-
+import { motion, Variants } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { ROUTES } from "@/config/constants";
+
+const slideUpVariants: Variants = {
+  hidden: { y: 50, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+};
 
 export const Hero = () => {
   return (
     <section id="hero" className="screen pt-24 space-y-11">
-      <h1 className="max-w-[1100px] font-medium capitalize text-2xl leading-[44px] sm:text-3xl sm:leading-[50px] md:text-4xl md:leading-[55px] lg:text-5xl lg:leading-[65px] xl:text-6xl xl:leading-[72px]">
+      <motion.h1
+        className="max-w-[1100px] font-medium capitalize text-2xl leading-[44px] sm:text-3xl sm:leading-[50px] md:text-4xl md:leading-[55px] lg:text-5xl lg:leading-[65px] xl:text-6xl xl:leading-[72px]"
+        variants={slideUpVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
         Maximize online efficiency with high-quality, affordable, worldwide{" "}
         <span className="relative inline-block">
           mobile proxies
@@ -20,20 +32,34 @@ export const Hero = () => {
             priority
           />
         </span>
-      </h1>
-      <p className="text-[#727282] capitalize text-base leading-[28px] sm:text-lg sm:leading-[29px] lg:text-xl lg:leading-[30px] max-w-[640px]">
+      </motion.h1>
+
+      <motion.p
+        className="text-[#727282] capitalize text-base leading-[28px] sm:text-lg sm:leading-[29px] lg:text-xl lg:leading-[30px] max-w-[640px]"
+        variants={slideUpVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
         Get ultra-stable mobile proxies with unlimited data and effortless
         country switching ⎯⎯ perfectly tailored for developers, social media
         managers, E-commerce, and digital marketing professionals.
-      </p>
-      <div className="flex flex-col md:flex-row items-center justify-center gap-3 w-full md:w-fit">
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col md:flex-row items-center justify-center gap-3 w-full md:w-fit"
+        variants={slideUpVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.6 }}
+      >
         <Button variant="outline" size="lg" className="w-full" asChild>
           <Link href={ROUTES.CONTACT_US}>Contact Sales</Link>
         </Button>
         <Button size="lg" className="-order-1 md:order-1 w-full" asChild>
           <Link href={ROUTES.DASHBOARD_HOME}>Get Started</Link>
         </Button>
-      </div>
+      </motion.div>
     </section>
   );
 };
