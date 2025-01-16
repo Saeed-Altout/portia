fb641bc4ee16ca781872943859963520c52e0274
Saeed Altout <saeedaltoutpro@gmail.com>
2025-01-16 13:33:27 +0300

    add animate to features

--- components/sections/features.tsx
+++ components/sections/features.tsx
@@ -1,6 +1,14 @@
-import { Heading } from "@/components/ui/heading";
-import { Circle, Icon } from "@/components/ui/circle-icon";
+"use client";
+
+import { motion, Variants } from "framer-motion";
 import { featuresData } from "@/config/constants";
+import { Heading } from "@/components/ui/heading";
+import { CircleIcon } from "@/components/circle-icon";
+
+const cardVariants: Variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 },
+};
 
 export const Features = () => {
   return (
@@ -11,17 +19,23 @@ export const Features = () => {
       />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16">
         {featuresData.map(({ title, description, icon }, index) => (
-          <div key={index} className="space-y-5">
-            <Circle>
-              <Icon icon={icon} />
-            </Circle>
+          <motion.div
+            key={index}
+            variants={cardVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
+            className="space-y-5"
+          >
+            <CircleIcon icon={icon} theme="primary" />
             <div className="space-y-2">
               <h2 className="text-[#0A0A0A] text-lg md:text-xl font-medium">
                 {title}
               </h2>
               <p className="text-[#727282]">{description}</p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
